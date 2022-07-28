fa704c76fa8a19e25d5c9b02244efd88b60fd29f
Spencer Hedger <spencer.hedger@outlook.com>
2022-07-28 19:05:09 +0100

    Test INFO log lines and case when no trim needed

--- test/test_remove_empty_rows_and_columns.py
+++ test/test_remove_empty_rows_and_columns.py
@@ -7,7 +7,8 @@ import logging
 from io import StringIO
 import remove_empty_rows_and_columns
 
-FILENAME_TEST = 'empty_rows_and_columns_test1.csv'
+FILENAME_TEST1 = 'empty_rows_and_columns_test1.csv'
+FILENAME_TEST2 = 'empty_rows_and_columns_test2.csv'
 
 
 class TestRemoveEmptyRowsAndColumns(unittest.TestCase):
@@ -17,18 +18,26 @@ class TestRemoveEmptyRowsAndColumns(unittest.TestCase):
         input_dir = os.path.join(file_dir, 'testdata/empty_rows_and_columns')
         output_dir = os.path.join(file_dir, 'out')
 
-        with self.assertLogs(level='WARNING') as cm:
+        with self.assertLogs() as cm:
             with unittest.mock.patch('sys.argv', ['test', '-i', input_dir, '-o', output_dir]):
                 remove_empty_rows_and_columns.main()
-                with open(os.path.join(output_dir, FILENAME_TEST)) as f:
+                with open(os.path.join(output_dir, FILENAME_TEST1)) as f:
                     fOut = f.read()
-                with open(os.path.join(file_dir, f'expected/{FILENAME_TEST}')) as f:
+                with open(os.path.join(file_dir, f'expected/{FILENAME_TEST1}')) as f:
                     fExpected = f.read()
                 self.assertEqual(fOut, fExpected,
-                                 msg=f'Comparing out/{FILENAME_TEST} and expected/{FILENAME_TEST}')
+                                 msg=f'Comparing out/{FILENAME_TEST1} and expected/{FILENAME_TEST1}')
 
-        warnings = [
+                with open(os.path.join(output_dir, FILENAME_TEST2)) as f:
+                    fOut = f.read()
+                with open(os.path.join(file_dir, f'expected/{FILENAME_TEST2}')) as f:
+                    fExpected = f.read()
+                self.assertEqual(fOut, fExpected,
+                                 msg=f'Comparing out/{FILENAME_TEST2} and expected/{FILENAME_TEST2}')
 
+        logs = [
+
+            r'empty_rows_and_columns_test2.csv: removed 0 columns and 0 rows',
             r'empty_rows_and_columns_test1.csv:2 extra data in cell 3: "4"',
             r'empty_rows_and_columns_test1.csv:2 extra data in cell 4: "5"',
             r'empty_rows_and_columns_test1.csv:2 extra data in cell 5: "6"',
@@ -39,8 +48,9 @@ class TestRemoveEmptyRowsAndColumns(unittest.TestCase):
             r'empty_rows_and_columns_test1.csv:11 extra data in cell 3: "D"',
             r'empty_rows_and_columns_test1.csv:11 extra data in cell 4: "E"',
             r'empty_rows_and_columns_test1.csv:11 extra data in cell 5: "F"',
+            r'empty_rows_and_columns_test1.csv: removed 3 columns and 6 rows'
         ]
 
-        self.assertEqual(len(warnings), len(cm.output))
+        self.assertEqual(len(logs), len(cm.output))
         for i, warning in enumerate(cm.output):
-            self.assertRegex(warning, warnings[i])
+            self.assertRegex(warning, logs[i])
